521d3202b8fe963144f2f3dfabac137c76f79f63
你最难得 <2810868696@qq.com>
2025-03-21 10:01:21 +0800

    tttt

--- otome-game-ai/src/router/index.js
+++ otome-game-ai/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 
 export const router = createRouter({
-  history: createWebHistory("/otomeGame/"),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
--- otome-game-ai/vite.config.js
+++ otome-game-ai/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
   return {
-    publicPath: '/otomeGame/',
+    base: './',
     plugins: [vue()],
     server: {
       host: '0.0.0.0',
